26532890c06c9b349bbec7393e3dcc680181b715
Bill Wang <bill.wang@bill.wang>
2024-10-03 13:43:11 +1000

    test with Github action

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ image=${1:-latest}
 for platform in linux/amd64 linux/arm/v7 linux/arm64/v8 linux/arm/v6 linux/ppc64le linux/s390x linux/386
 do
   echo "Platfom is $platform"
-  docker run --rm -ti --platform "${platform}" --entrypoint=sh ${image} -c "uname -m"
+  docker run --rm -i --platform "${platform}" --entrypoint=sh ${image} -c "uname -m"
 done

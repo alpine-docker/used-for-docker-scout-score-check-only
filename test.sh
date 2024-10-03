f52c25659fa72b52bf0ac38fd4f7af67a0dba729
Bill Wang <bill.wang@bill.wang>
2024-10-03 11:46:00 +1000

    test with Github action

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-tag=${1:-latest}
-image="alpine/used-for-docker-scout-score-check-only"
+image=${1:-latest}
 
 for platform in linux/amd64 linux/arm/v7 linux/arm64/v8 linux/arm/v6 linux/ppc64le linux/s390x linux/386
 do
   echo "Platfom is $platform"
-  docker run --rm -ti --platform "${platform}" --entrypoint=sh ${image}:${tag} -c "uname -m"
+  docker run --rm -ti --platform "${platform}" --entrypoint=sh ${image} -c "uname -m"
 done
